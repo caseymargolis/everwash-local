99635d4b30937baa4afc1a209f49eb658af785c9
Snail0917 <wangjinzh1991@gmail.com>
2023-12-30 06:31:17 +0800

    wash partner growth

--- src/js/main.js
+++ src/js/main.js
@@ -892,4 +892,28 @@ $(function() {
   });
 });
 
+$('.owl-carousel.video-items').owlCarousel({
+  merge:true,
+  loop:true,
+  margin:20,
+  video:true,
+  lazyLoad:true,
+  center:true,
+  responsive:{
+    0:{
+        items:1,
+        nav:true
+    },
+    600:{
+        items:2,
+        nav:false
+    },
+    1000:{
+        items:3,
+        nav:true,
+        loop:false
+    }
+  }
+})
+
 
