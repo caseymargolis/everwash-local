eeaba62c8a3cc2eb8a52ee3472a882a3216b8ae4
Snail0917 <wangjinzh1991@gmail.com>
2024-01-16 09:51:43 +0800

    some update 8

--- src/js/main.js
+++ src/js/main.js
@@ -960,27 +960,9 @@ document.getElementById('washOwnerPopup').addEventListener('submit', async funct
       body: formData,
     });
 
+    console.log('netlify response:', netlifyResponse);
     // Check if the Netlify form submission was successful
-    if (netlifyResponse.ok) {
-        // Send additional data to the external server
-      const externalServerResponse = await fetch('https://go.everwash.com/l/996891/2024-01-05/zf7r', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(Object.fromEntries(formData)),
-      });
 
-      // Check if the external server request was successful
-      if (externalServerResponse.ok) {
-        console.log('Form submitted successfully to Netlify and external server.');
-        // Optionally redirect or perform other actions
-      } else {
-        console.error('Error submitting to external server:', externalServerResponse.statusText);
-      }
-    } else {
-      console.error('Error submitting to Netlify:', netlifyResponse.statusText);
-    }
   } catch (error) {
     console.error('Error:', error);
   }
