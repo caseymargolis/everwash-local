218da2835be45de513f01ff881bc07d88ae66910
Snail0917 <wangjinzh1991@gmail.com>
2024-01-18 05:05:35 +0800

    jjj

--- netlify/functions/submitForm.js
+++ netlify/functions/submitForm.js
@@ -4,10 +4,7 @@ import { cors } from '@netlify/functions';
 exports.handler = cors(async function (event, context) {
   try {
     // Process the initial form data or perform any additional actions here
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Form submitted successfully!' }),
-    };
+    return Response.json({ message: 'Form submitted successfully!' });
   } catch (error) {
     console.error('Error processing form submission:', error);
 
