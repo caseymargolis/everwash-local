bc23571c167fce65f915d1ef316f0ecdaf8ccb2b
Snail0917 <wangjinzh1991@gmail.com>
2024-01-18 09:03:55 +0800

    Website Update 30

--- netlify/functions/submitForm.js
+++ netlify/functions/submitForm.js
@@ -2,48 +2,37 @@
 import { cors } from '@netlify/functions';
 const fetch = require('node-fetch');
 
-exports.handler = async function (event, context) {
+exports.handler = cors(async function (event, context) {
   try {
-    // // Extract data from the incoming request if needed
+    // Extract data from the incoming request if needed
     const requestData = JSON.parse(event.body);
 
-    // // Replace 'https://external-server.com/api' with the actual API endpoint
-    // const apiUrl = 'https://go.everwash.com/l/996891/2024-01-05/zf7r';
+    // Replace 'https://external-server.com/api' with the actual API endpoint
+    const apiUrl = 'https://go.everwash.com/l/996891/2024-01-05/zf7r';
 
-    // // Options for the fetch request (including method, headers, and body)
-    // const requestOptions = {
-    //   method: 'POST', // or 'GET', 'PUT', etc.
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     // Add any additional headers required by the external server
-    //   },
-    //   body: JSON.stringify(requestData),
-    // };
-
-    // // Perform the fetch request
-    // const response = await fetch(apiUrl, requestOptions);
-
-    // if (!response.ok) {
-    //   throw new Error(`HTTP error! Status: ${response.status}`);
-    // }
-
-    // // Parse and return the response data
-    // const responseData = await response.json();
-
-    fetch('https://go.everwash.com/l/996891/2024-01-05/zf7r', {
-      method: 'POST',
+    // Options for the fetch request (including method, headers, and body)
+    const requestOptions = {
+      method: 'POST', // or 'GET', 'PUT', etc.
       headers: {
         'Content-Type': 'application/json',
+        // Add any additional headers required by the external server
       },
       body: JSON.stringify(requestData),
-    })
-      .then(response => response.json())
-      .then(data => console.log('Response from external server:', data))
-      .catch(error => console.error('Error:', error));
+    };
+
+    // Perform the fetch request
+    const response = await fetch(apiUrl, requestOptions);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    // Parse and return the response data
+    const responseData = await response.json();
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: requestData }),
+      body: JSON.stringify(responseData),
     };
   } catch (error) {
     console.error('Error processing form submission:', error);
@@ -52,5 +41,5 @@ exports.handler = async function (event, context) {
       body: JSON.stringify({ error: error.message }),
     };
   }
-};
+});
   
