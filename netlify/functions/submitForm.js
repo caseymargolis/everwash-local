df988548748b728adaae633e7587a0052c3ff5bf
Snail0917 <wangjinzh1991@gmail.com>
2024-01-18 09:16:45 +0800

    kkkkk

--- netlify/functions/submitForm.js
+++ netlify/functions/submitForm.js
@@ -1,39 +1,85 @@
 // submitForm.js
 import { cors } from '@netlify/functions';
-const fetch = require('node-fetch');
+const https = require('https');
 
 exports.handler = cors(async function (event, context) {
   try {
     // Extract data from the incoming request if needed
     const requestData = JSON.parse(event.body);
+    const data = JSON.stringify(requestData);
 
-    // Replace 'https://external-server.com/api' with the actual API endpoint
-    const apiUrl = 'https://go.everwash.com/l/996891/2024-01-05/zf7r';
-
-    // Options for the fetch request (including method, headers, and body)
-    const requestOptions = {
-      method: 'POST', // or 'GET', 'PUT', etc.
+    const options = {
+      hostname: 'go.everwash.com',
+      port: 443,
+      path: '/l/996891/2024-01-05/zf7r',
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        // Add any additional headers required by the external server
+        'Content-Length': data.length,
+        // Add any additional headers required by the API
       },
-      body: JSON.stringify(requestData),
     };
 
-    // Perform the fetch request
-    const response = await fetch(apiUrl, requestOptions);
+    const req = https.request(options, (res) => {
+      let responseData  = '';
+    
+      // A chunk of data has been received.
+      res.on('data', (chunk) => {
+        responseData  += chunk;
+      });
+    
+      // The whole response has been received.
+      res.on('end', () => {
+        console.log(responseData);
+            return {
+            statusCode: 200,
+            body: responseData,
+          };
+      });
+    });
+    
+    req.on('error', (error) => {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          error: error.message || "Something went wrong with 3rd party API"
+        }),
+      };
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
+      
+    });
+    
+    // Send the POST request with the data
+    req.write(data);
+    req.end();
 
-    // Parse and return the response data
-    const responseData = await response.json();
+    // // Replace 'https://external-server.com/api' with the actual API endpoint
+    // const apiUrl = 'https://go.everwash.com/l/996891/2024-01-05/zf7r';
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(responseData),
-    };
+    // // Options for the fetch request (including method, headers, and body)
+    // const requestOptions = {
+    //   method: 'POST', // or 'GET', 'PUT', etc.
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     // Add any additional headers required by the external server
+    //   },
+    //   body: JSON.stringify(requestData),
+    // };
+
+    // // Perform the fetch request
+    // const response = await fetch(apiUrl, requestOptions);
+
+    // if (!response.ok) {
+    //   throw new Error(`HTTP error! Status: ${response.status}`);
+    // }
+
+    // // Parse and return the response data
+    // const responseData = await response.json();
+
+    // return {
+    //   statusCode: 200,
+    //   body: JSON.stringify(responseData),
+    // };
   } catch (error) {
     console.error('Error processing form submission:', error);
     return {
