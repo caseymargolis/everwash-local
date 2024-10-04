a81f11319eafb54333abddbccbe4cfc746eda4ec
Snail0917 <wangjinzh1991@gmail.com>
2024-10-04 13:38:30 +0800

    website 1/35

--- src/js/mailer.js
+++ src/js/mailer.js
@@ -3,7 +3,7 @@
 window.onload = function() {
   var buttons = document.querySelectorAll('.cta-options-btn a');
   if (buttons.length === 0) {
-    console.error('No buttons found!');
+    console.log('No buttons found!');
     return; // Exit if no buttons are found
   }
   buttons.forEach(function(button) {
