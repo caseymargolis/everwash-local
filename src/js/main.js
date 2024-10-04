0a359524305ea711b9780f64deb3473c7da3209e
Snail0917 <wangjinzh1991@gmail.com>
2024-10-04 13:16:32 +0800

    website 1/16

--- src/js/main.js
+++ src/js/main.js
@@ -985,6 +985,10 @@ $(function() {
 // Randomly button display on washowner page banner...
 window.onload = function() {
   var buttons = document.querySelectorAll('.cta-options-btn a');
+  if (buttons.length === 0) {
+    console.error('No buttons found!');
+    return; // Exit if no buttons are found
+  }
   buttons.forEach(function(button) {
     button.style.display = 'none';
   });
