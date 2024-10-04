7f8798d09df8866e3c2d5a8ef30aab8b9afb4f0f
Snail0917 <wangjinzh1991@gmail.com>
2024-10-04 13:22:56 +0800

    website 1/20

--- src/js/main.js
+++ src/js/main.js
@@ -983,18 +983,18 @@ $(function() {
 
 
 // Randomly button display on washowner page banner...
-window.onload = function() {
-  var buttons = document.querySelectorAll('.cta-options-btn a');
-  if (buttons.length === 0) {
-    console.log('No buttons found!');
-    return; // Exit if no buttons are found
-  }
-  buttons.forEach(function(button) {
-    button.style.display = 'none';
-  });
-  var randomIndex = Math.floor(Math.random() * buttons.length);
-  buttons[randomIndex].style.display = 'block';
-};
+// window.onload = function() {
+//   var buttons = document.querySelectorAll('.cta-options-btn a');
+//   if (buttons.length === 0) {
+//     console.log('No buttons found!');
+//     return; // Exit if no buttons are found
+//   }
+//   buttons.forEach(function(button) {
+//     button.style.display = 'none';
+//   });
+//   var randomIndex = Math.floor(Math.random() * buttons.length);
+//   buttons[randomIndex].style.display = 'block';
+// };
 
 
 // popup form submit to external action link & netlify
